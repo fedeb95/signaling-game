9863159a5279e6d2c9248d0d190dfd9e7d2356de
Federico Bonfiglio <federico.bonfiglio@protonmail.ch>
2022-07-02 16:54:54 +0200

    fix prints

--- signaling.py
+++ signaling.py
@@ -43,34 +43,59 @@ class Receiver(UrnLearner):
     def act(self, signal):
         return self.choose_ball(signal)
 
-def positive_strategy(state, sender, receiver):
-    signal = sender.signal(state)
-    act = receiver.act(signal)
-    if state == act:
-        sender.put_ball(state, signal)    
-        receiver.put_ball(signal, state)
-        return True
-    return False
+class Strategy:
+    def apply(self, state, sender, receiver):
+        raise NotImplementedError
 
-def positive_negative_strategy(state, sender, receiver): 
-    signal = sender.signal(state)
-    act = receiver.act(signal)
-    if state == act:
-        sender.put_ball(state, signal)    
-        receiver.put_ball(signal, state)
-        return True
-    else:
-        sender.remove_but_one(state, signal)
-        receiver.remove_but_one(signal, state)
+class PositiveStrategy(Strategy):
+    def apply(self, state, sender, receiver):
+        signal = sender.signal(state)
+        act = receiver.act(signal)
+        if state == act:
+            sender.put_ball(state, signal)    
+            receiver.put_ball(signal, state)
+            return True
         return False
 
+class PositiveNegativeStrategy(PositiveStrategy):
+    def apply(self, state, sender, receiver):
+        signal = sender.signal(state)
+        act = receiver.act(signal)
+        if state == act:
+            sender.put_ball(state, signal)    
+            receiver.put_ball(signal, state)
+            return True
+        else:
+            sender.remove_but_one(state, signal)
+            receiver.remove_but_one(signal, state)
+            return False
+
+def randomize_urn_learner(learner, interval):
+    a = interval[0]
+    b = interval[1]
+    for urn_type in learner.urn_types:
+        for ball_type in learner.ball_types:
+            randomized = learner.urns[urn_type][ball_type] * random.uniform(a, b)
+            learner.urns[urn_type][ball_type] = randomized
+
+class RandPositiveStrategy(PositiveStrategy):
+    def __init__(self, interval):
+        self.interval = interval 
+
+    def apply(self, state, sender, receiver):
+        result = super().apply(state, sender, receiver)
+        randomize_urn_learner(sender, self.interval) 
+        randomize_urn_learner(receiver, self.interval) 
+        return result
+    
+            
 def main():
     parser = argparse.ArgumentParser(description='A Lewis singaling game simulation')
     parser.add_argument('-r', '--runs', dest='runs', action='store', default='1000', help='Number of runs', type=int)
     parser.add_argument('-s', '--states', dest='states', action='store', default='2', help='Number of states', type=int)
     parser.add_argument('-t', '--terms', dest='terms', action='store', default='2', help='Number of terms (signals)', type=int)
     parser.add_argument('-l', '--learning', dest='learning', action='store', default='positive', help='Type of learning', type=str,
-        choices=['positive', 'positive_negative'])
+        choices=['positive', 'positive_negative', 'randomized'])
     args = parser.parse_args()
 
     states = [ i for i in range(0, args.states) ]
@@ -82,13 +107,15 @@ def main():
     for i in range(0, args.runs):
         state = random.choice(states) 
         if args.learning == 'positive':
-            strategy = positive_strategy
+            strategy = PositiveStrategy()
         elif args.learning == 'positive_negative':
-            strategy = positive_negative_strategy
+            strategy = PositiveNegativeStrategy() 
+        elif args.learning == 'randomized':
+            strategy = RandPositiveStrategy((0.8, 1.2))
         else:
-            raise TypeError('Unknown payoff type, supported: positive, positive_negative')
+            raise TypeError('Unknown payoff type, supported: positive, positive_negative, randomized')
 
-        if strategy(state, sender, receiver):
+        if strategy.apply(state, sender, receiver):
             successes += 1
         total += 1
 
@@ -96,13 +123,13 @@ def main():
     for st in states:
         print(f'\tState {st}:')
         for sg in signals:
-            count = len([ el for el in sender.urns[st] if el == sg ])
+            count = sender.urns[sg][st]
             print(f'\t\tSignal {sg} count: {count}') 
     print('Receiver:')
     for sg in signals:
         print(f'\tSignal {sg}:')
         for st in states:
-            count = len([ el for el in receiver.urns[sg] if el == st ])
+            count = receiver.urns[st][sg]
             print(f'\t\tState {st} count: {count}') 
     print(f'Success rate: {successes/total}')
 
